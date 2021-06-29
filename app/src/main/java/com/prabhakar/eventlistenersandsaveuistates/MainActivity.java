45ebb48b46c4e75d8b8ef7e64e3a32f7589152f2
Prabhakar <prabhakarkr802@gmail.com>
2021-06-29 17:23:56 +0530

    Event Listeners and Save UI States_YOU(Assignment MASAI School)

---
+++ app/src/main/java/com/prabhakar/eventlistenersandsaveuistates/MainActivity.java
@@ -0,0 +1,31 @@
+package com.prabhakar.eventlistenersandsaveuistates;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text);
+        textView.setText("Hey Masai");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("msg","Screen Rotation");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        textView.setText(savedInstanceState.getString("msg"));
+    }
+}
